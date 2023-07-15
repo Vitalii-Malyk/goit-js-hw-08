8c219ab67b32fb7daebcf3bed4305d7274164dbd
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-07-15 13:38:45 +0300

    fix 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,41 @@
 import throttle from 'lodash.throttle';
 
-const formEmailEl = document.querySelector('.feedback-form [name=email]');
-const formMessageEl = document.querySelector('.feedback-form [name=message]');
+const formEl = document.querySelector('.feedback-form');
+
 const formSubmitEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-const feedbackForm = {
-  email: '',
-  message: '',
-};
+const feedbackForm = {};
 
 populateInput();
 
-formEmailEl.addEventListener('input', throttle(onInputChangeEmail, 500));
-formMessageEl.addEventListener('input', throttle(onInputChangeMessage, 500));
-formSubmitEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onInputChange, 500));
+formEl.addEventListener('submit', onFormSubmit);
 
-function onInputChangeEmail(event) {
-  feedbackForm.email = event.target.value;
-
+function onInputChange(event) {
+  feedbackForm[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
 }
-function onInputChangeMessage(event) {
-  feedbackForm.message = event.target.value;
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
-}
 function onFormSubmit(event) {
   event.preventDefault();
+  const {
+    elements: { email, message },
+  } = eventCurrentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all fields of the form');
+  }
   event.currentTarget.reset();
+  feedbackForm = {};
   localStorage.removeItem(STORAGE_KEY);
-  const feedbackForm = {
-    email: '',
-    message: '',
-  };
-  console.log(feedbackForm);
 }
 
 function populateInput() {
-  const saveInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
+  const saveInput = localStorage.getItem(STORAGE_KEY);
   if (saveInput) {
-    formEmailEl.value = saveInput.email;
-    formMessageEl.value = saveInput.message;
+    feedbackForm = JSON.parse(saveInput);
+    for (let key in feedbackForm) {
+      formEl[key].value = feedbackForm[key];
+    }
   }
 }
